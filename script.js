c3ae1cb02094d998de326fb71a95f922e74018a7
kol3x <kolaegipet@gmail.com>
2023-08-15 17:56:42 +0300

    Added some styles, but still work in progresss

--- script.js
+++ script.js
@@ -1,70 +1,67 @@
 let myLibrary = [];
 
 class Book {
-    constructor(author, title, numberOfPages, isRead) {
-        this.author = author;
-        this.title = title;
-        this.numberOfPages = numberOfPages;
-        this.isRead = isRead;
-    }
+  constructor(author, title, numberOfPages, isRead) {
+    this.author = author;
+    this.title = title;
+    this.numberOfPages = numberOfPages;
+    this.isRead = isRead;
+  }
 }
-
 
 function showBooks() {
-    // Clear table
-    document.querySelectorAll('.book-tr').forEach(tableRow => {
-        tableRow.remove();
+  // Clear table
+  document.querySelectorAll(".book-tr").forEach((tableRow) => {
+    tableRow.remove();
+  });
+  // Create new table
+  myLibrary.forEach((book, index) => {
+    if (book == undefined) return 0;
+    let table = document.querySelector("table");
+    let tablerow = document.createElement("tr");
+    tablerow.classList.add("book-tr");
+
+    Object.keys(book).forEach((key) => {
+      let tabledata = document.createElement("td");
+      tablerow.appendChild(tabledata);
+      tabledata.textContent = book[key];
     });
-    // Create new table
-    myLibrary.forEach((book, index) => {  
-        if (book == undefined) return 0;
-        let table = document.querySelector('table');
-        let tablerow = document.createElement('tr');
-        tablerow.classList.add('book-tr');
 
-        Object.keys(book).forEach(key => {
-            let tabledata = document.createElement('td');
-            tablerow.appendChild(tabledata);
-            tabledata.textContent = book[key];
-        });
+    removeButton = document.createElement("button");
+    removeButton.textContent = "remove";
 
-        removeButton = document.createElement('button');
-        removeButton.textContent = 'remove';
-
-        removeButton.addEventListener('click', function() {
-            delete myLibrary[index];
-            showBooks();
-        });
-
-        changeReadButton = document.createElement('button');
-        changeReadButton.textContent = 'Change read status';
-
-        changeReadButton.addEventListener('click', function() {
-            myLibrary[index].isRead = !(myLibrary[index].isRead);
-            showBooks();
-        });
-
-        tablerow.append(removeButton, changeReadButton)
-
-        table.appendChild(tablerow);
+    removeButton.addEventListener("click", function () {
+      delete myLibrary[index];
+      showBooks();
     });
+
+    changeReadButton = document.createElement("button");
+    changeReadButton.textContent = "Change read status";
+
+    changeReadButton.addEventListener("click", function () {
+      myLibrary[index].isRead = !myLibrary[index].isRead;
+      showBooks();
+    });
+
+    tablerow.append(removeButton, changeReadButton);
+
+    table.appendChild(tablerow);
+  });
 }
-
 
 function addBookToLibrary(author, title, num, isRead) {
-    myLibrary.push(new Book(author, title, num, isRead));
-    showBooks();
+  myLibrary.push(new Book(author, title, num, isRead));
+  showBooks();
 }
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    newBookButton = document.querySelector('#add');
-    formDiv = document.createElement('div');
-    formDiv.classList.add('form-div');
-    // Create a form on button hit
-    newBookButton.addEventListener('click', function() {
-        document.querySelector('body').appendChild(formDiv);
-        document.querySelector('.form-div').innerHTML = `
+document.addEventListener("DOMContentLoaded", function () {
+  newBookButton = document.querySelector("#add");
+  formDiv = document.createElement("div");
+  formDiv.classList.add("form-div");
+  // Create a form on button hit
+  newBookButton.addEventListener("click", function () {
+    document.querySelector("body").appendChild(formDiv);
+    document.querySelector(".form-div").innerHTML = `
             <form id='new-book'>
             Book name:
                 <input id='book-name'>  <br>
@@ -76,25 +73,25 @@ document.addEventListener('DOMContentLoaded', function() {
                 <input id='book-read' type='checkbox'> 
                 <button type='submit'>Submit a book</button>
             </form>   
-        `;   
-    newBookButton.style.display = 'none';
-    });
-    // Handle form submition
-    formDiv.addEventListener('submit', function(event) {
-        // makes sure form submition doesn't update the page
-        event.preventDefault();
+        `;
+    newBookButton.style.display = "none";
+  });
+  // Handle form submition
+  formDiv.addEventListener("submit", function (event) {
+    // makes sure form submition doesn't update the page
+    event.preventDefault();
 
-        isReadForm = document.querySelector('#book-read');
-        let isRead = false;
-        if (isReadForm.checked) isRead = true;
+    isReadForm = document.querySelector("#book-read");
+    let isRead = false;
+    if (isReadForm.checked) isRead = true;
 
-        addBookToLibrary(
-            document.querySelector('#book-name').value,
-            document.querySelector('#book-author').value,
-            document.querySelector('#book-pages').value,
-            isRead
-        );
-        newBookButton.style.display = '';
-        formDiv.remove();   
-    });
+    addBookToLibrary(
+      document.querySelector("#book-name").value,
+      document.querySelector("#book-author").value,
+      document.querySelector("#book-pages").value,
+      isRead
+    );
+    newBookButton.style.display = "";
+    formDiv.remove();
+  });
 });
